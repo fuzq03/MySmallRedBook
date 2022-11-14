87598e8f6ae615bb5f4f7d9c49384f21c52774df
fuzq03 <1594649408@qq.com>
2022-11-15 00:18:49 +0800

    完成了我页面的基本设置

---
+++ SmallRedBooks-main/app/src/main/java/com/example/smallredbook/ui/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.smallredbook.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.smallredbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
